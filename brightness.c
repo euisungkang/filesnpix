4c47166664950362a798316d97c6cb91aa9b3dbb
Euisung Kang <ekang05@vm-hw09.eecs.tufts.edu>
2019-09-12 17:52:03 -0400

    brigthness update

--- brightness.c
+++ brightness.c
@@ -9,7 +9,7 @@ PURPOSE: Part A of assignment. This short program reads in an image and
          outputs the average brightness of every pixel in the image
 NOTE:    
 
-Last Modified: 09/09/2019
+Last Modified: 09/10/2019
 */
 
 #include <stdio.h>
@@ -21,35 +21,50 @@ double average_brightness(void *);
 
 int main (int argc, char *argv[])
 {
-    printf("%u\n", argc);
     //Error message, too many arguments
     if (argc > 2) {
-        fprintf(stderr, "%s\n", "Invalid argument(s) for file");
+        fprintf(stderr, "Invalid input files: ./brightness [image file]");
         return 0;
     }
 
-    void *grayscale;
-    //No arguments, read from stdin
+    void *grayscale;        //void * to store pnmrdr
+
+    //If image file is not given, read from stdin instead
     if (argc == 1) { 
+
+        //Initialize pnmrdr with stdin stream, display brightness
         grayscale = Pnmrdr_new(stdin);
         printf("%f\n", average_brightness(grayscale));
+
     } else if (argc == 2) {
+
         FILE *img;
-        open_file(&img, argv[1]);
+        open_file(&img, argv[1]);       //Open file
+
+        //Initialize pnmrdr with file stream, display brightness
         grayscale = Pnmrdr_new(img);
-        if (img)
-            printf("%s\n", "file has been opened");
-        else 
-            printf("%s\n", "file has not been opened");
         printf("%f\n", average_brightness(grayscale)); 
-        fclose(img);
+
+        fclose(img);        //Close to avoid mem leak
+    }
+
+    return 0;
+}
+
+/*
+PURPOSE: Will open given file and return a pointer to it. Will display error
+         message and exit program if file can't be opened       */
+void open_file(FILE **img, char *filename) {
+    *img = fopen(filename, "r");
+    if (!img) {
+        fprintf(stderr, "Error: file could not be opened");
+        exit(4);
     }
 }
 
-void open_file(FILE **img, char *filename) {
-    *img = fopen(filename, "r");
-}
-
+/*
+PURPOSE: Returns the average brightness of a given pgm file. Iterates
+         every pixel's brightness    */
 double average_brightness(void *img) {
     Pnmrdr_mapdata data = Pnmrdr_data(img);
     double pixelResult = 0;
